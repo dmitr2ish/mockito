663c0835c82bb28ddac0b2159387375b1bb93b8e
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2022-03-20 15:45:35 +0300

    mockito junit5 fake case

--- src/test/java/dmitr2ish/com/github/mockito/test_doubles/fake/FakeTest.java
+++ src/test/java/dmitr2ish/com/github/mockito/test_doubles/fake/FakeTest.java
@@ -3,8 +3,11 @@ package dmitr2ish.com.github.mockito.test_doubles.fake;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class FakeTest {
 
@@ -13,9 +16,28 @@ class FakeTest {
         FBookRepository FBookRepository = new FakeFBookRepository();
         FBookService FBookService = new FBookService(FBookRepository);
 
-        FBookService.addBook(new FBook("1234","Mockito in Action",250, LocalDate.now()));
-        FBookService.addBook(new FBook("1235","JUnit in Action",200, LocalDate.now()));
+        FBookService.addBook(new FBook("1234", "Mockito in Action", 250, LocalDate.now()));
+        FBookService.addBook(new FBook("1235", "JUnit in Action", 200, LocalDate.now()));
 
         assertEquals(2, FBookService.findNumberOfBooks());
     }
+
+    @Test
+    void testFakeWithMockito() {
+        FBookRepository bookRepository = mock(FBookRepository.class);
+        FBookService bookService = new FBookService(bookRepository);
+
+        FBook book1 = new FBook("1234", "Mockito in Action", 250, LocalDate.now());
+        FBook book2 = new FBook("1235", "JUnit in Action", 200, LocalDate.now());
+
+        Collection<FBook> collectionBooks = new ArrayList<>();
+
+        collectionBooks.add(book1);
+        collectionBooks.add(book2);
+
+        when(bookRepository.findAll()).thenReturn(collectionBooks);
+
+        assertEquals(2, bookService.findNumberOfBooks());
+    }
 }
+
