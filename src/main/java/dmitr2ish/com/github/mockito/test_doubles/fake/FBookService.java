b4d56b289708b8d9a6be09d56702312c67576f59
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2022-03-20 19:29:35 +0300

    mockito junit5 mock case

--- src/main/java/dmitr2ish/com/github/mockito/test_doubles/fake/FBookService.java
+++ src/main/java/dmitr2ish/com/github/mockito/test_doubles/fake/FBookService.java
@@ -2,17 +2,20 @@ package dmitr2ish.com.github.mockito.test_doubles.fake;
 
 public class FBookService {
 
-    private FBookRepository FBookRepository;
+    private FBookRepository fBookRepository;
 
-    public FBookService(FBookRepository FBookRepository) {
-        this.FBookRepository = FBookRepository;
+    public FBookService(FBookRepository fBookRepository) {
+        this.fBookRepository = fBookRepository;
     }
 
-    public void addBook(FBook FBook) {
-        FBookRepository.save(FBook);
+    public void addBook(FBook fBook) {
+        if (fBook.getPrice() > 400) {
+            return;
+        }
+        fBookRepository.save(fBook);
     }
 
     public int findNumberOfBooks() {
-        return FBookRepository.findAll().size();
+        return fBookRepository.findAll().size();
     }
 }
--- src/test/java/dmitr2ish/com/github/mockito/test_doubles/mock/MockTest.java
+++ src/test/java/dmitr2ish/com/github/mockito/test_doubles/mock/MockTest.java
@@ -1,8 +1,13 @@
 package dmitr2ish.com.github.mockito.test_doubles.mock;
 
+import dmitr2ish.com.github.mockito.test_doubles.fake.FBook;
+import dmitr2ish.com.github.mockito.test_doubles.fake.FBookRepository;
+import dmitr2ish.com.github.mockito.test_doubles.fake.FBookService;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.mockito.Mockito.*;
 
 class MockTest {
 
@@ -18,5 +23,24 @@ class MockTest {
         bookService.addBook(mBook2); //save will be called
 
         bookRepository.verify(mBook2, 1);
+    }
+
+    @Test
+    void demoMockWithMockito() {
+        FBookRepository bookRepository = mock(FBookRepository.class);
+        FBookService bookService = new FBookService(bookRepository);
+
+        FBook mBook1 = new FBook("1234", "Mockito in Action", 500, LocalDate.now());
+        FBook mBook2 = new FBook("1235", "JUnit in Action", 400, LocalDate.now());
+
+        bookService.addBook(mBook1); //return
+        bookService.addBook(mBook2); //save will be called
+
+        //checking that really saved second book
+        verify(bookRepository).save(mBook2);
+
+        //checking how much times was called save
+        verify(bookRepository, times(0)).save(mBook1);
+        verify(bookRepository, times(1)).save(mBook2);
     }
 }
